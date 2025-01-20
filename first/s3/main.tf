13ef96f75113bff98723f196a62ed0730dee72ab
max <paulmaxchamblain@gmail.com>
2025-01-20 11:30:24 -0500

    add s3 bucket

---
+++ first/s3/main.tf
@@ -0,0 +1,18 @@
+# Define the AWS provider and set the region
+provider "aws" {
+  region = "us-east-1"
+}
+
+// Create an S3 bucket resource
+resource "aws_s3_bucket" "Titans_pic_2025" {
+
+  
+  // Set the access control list to private
+  acl    = "private"
+
+  // Add tags to the bucket
+  tags = {
+    Name        = "My bucket"       // Name tag
+    Environment = "Dev"             // Environment tag
+  }
+}
